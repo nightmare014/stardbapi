e9142bd1c0c77e3b7e74c68dc396979d12f6b715
nightmare014 <timanuriev@list.ru>
2025-03-05 09:18:02 +0500

    step 9 added error catcher component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,52 +1,39 @@
 import React, {Component} from 'react'
-import SwapiService from "../../services/swapi-service";
+
 import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ItemList from '../item-list'
 import PersonDetails from '../person-details'
+
+import './app.css'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
-import './app.css'
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component{
+
     swapiService = new SwapiService()
+
     state = {
         selectedPerson: null,
         hasError: false
-        }
-        componentDidCatch(error, errorInfo) {
-            this.setState({ hasError: true })
     }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true })
+    }
+
     render() {
+
         if (this.state.hasError) {
             return <ErrorIndicator />
         }
+
         return (
             <div>
                 <Header/>
                 <RandomPlanet/>
                 <PeoplePage />
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList
-                            onItemSelected={this.onPersonSelected}
-                            getData={ this.swapiService.getAllPlanets }
-                            renderItem={ (item) => item.name }/>                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
-                </div>
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList
-                            onItemSelected={this.onPersonSelected}
-                            getData={ this.swapiService.getAllStarships }
-                            renderItem={ (item) => item.name }/>                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
-                </div>
             </div>
         )
     }
