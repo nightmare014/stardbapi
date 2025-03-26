5b461799f259849ce92d116eafac93512809dc17
nightmare014 <timanuriev@list.ru>
2025-03-26 09:35:33 +0500

    step 18 refactoring

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom'
-import './header.css';
-
+import './header.css'
 const Header = () => {
   return (
     <div className="header d-flex">
-      <h3>
-      <Link to="/">StarDB</Link>
-      </h3>
-      <ul className="d-flex">
-        <li>
-        <Link to="/people/">People</Link>        </li>
-        <li>
-        <Link to="/planets/">Planets</Link>        </li>
-        <li>
-        <Link to="/starships/">Starships</Link>        </li>
-      </ul>
+        <h3>
+            <a href="/">
+            StarDB
+            </a>
+        </h3>
+        <ul className="d-flex">
+            <li>
+                <a href="/people">People</a>
+            </li>
+            <li>
+                <a href="/planets">Planets</a>
+            </li>
+            <li>
+                <a href="/starships">Starships</a>
+            </li>
+        </ul>
     </div>
-  );
-};
-
-export default Header;
+)
+}
+export default Header
