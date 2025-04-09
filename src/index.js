54b8984dd21ce3a2cd6a45ad4a22336269a43d4d
nightmare014 <timanuriev@list.ru>
2025-04-09 11:12:02 +0500

    step 26 refactroring starships page and welcome page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/app'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
